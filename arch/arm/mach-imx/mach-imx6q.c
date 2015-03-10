f4d72f785979f27013380e05b6b8560048e383eb
Sam Nazarko <email@samnazarko.co.uk>
2015-03-10 14:57:42 +0000

    Add initial implementation of Device Tree for Vero Revision 1.0 (production)
    We keep this kernel fairly vanilla so it's easier to track upstream. Some downstream patches wouldn't make sense in all use scenarios: i.e. the Buildroot installer.
    
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- arch/arm/mach-imx/mach-imx6q.c
+++ arch/arm/mach-imx/mach-imx6q.c
@@ -452,7 +452,7 @@ static const char *imx6q_dt_compat[] __initconst = {
 	NULL,
 };
 
-DT_MACHINE_START(IMX6Q, "Freescale i.MX6 Quad/DualLite (Device Tree)")
+DT_MACHINE_START(IMX6Q, "Vero")
 	/*
 	 * i.MX6Q/DL maps system memory at 0x10000000 (offset 256MiB), and
 	 * GPU has a limit on physical address that it accesses, which must
